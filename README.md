a147d02d5c0650cf158a36e974ea1c199eb74993
houdunwang <2300071698@qq.com>
2017-08-14 23:54:40 +0800

    no message

--- README.md
+++
@@ -1,2 +0,0 @@
-# hdjs-webpack
-模块化前端组件库
---
+++ static/requirejs/config.js
@@ -0,0 +1,29 @@
+/**
+ * 前端模块配置
+ * @author 向军 <2300071698@qq.com>
+ */
+require.config({
+    baseUrl: window.hdjs.base,
+    paths: {
+        'jquery':'https://cdn.bootcss.com/jquery/3.2.1/jquery.min',
+        'util':'util',
+        'css': 'static/requirejs/css.min',
+    },
+    shim: {
+        'util': {
+            deps: ['css!app.css']
+        },
+    },
+    waitSeconds: 30
+});
+
+
+
+
+
+
+
+
+
+
+
