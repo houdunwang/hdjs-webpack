184d51b48caacacf631384d57c609b5793d606a6
houdunwang <2300071698@qq.com>
2017-08-26 16:58:53 +0800

    no message

--- static/requirejs/config.js
+++ static/requirejs/config.js
@@ -2,16 +2,25 @@
  * 前端模块配置
  * @author 向军 <2300071698@qq.com>
  */
+if (!window.hdjs) {
+    window.hdjs = {'base': '', 'uploader': '', 'filesLists': ''};
+}
+window.hdjs_path = window.hdjs.base ? window.hdjs.base : '/node_modules/hdjs';
 require.config({
-    baseUrl: window.hdjs.base,
+    baseUrl: hdjs_path,
     paths: {
-        'jquery':'https://cdn.bootcss.com/jquery/3.2.1/jquery.min',
-        'util':'util',
-        'css': 'static/requirejs/css.min',
+        jquery: 'https://cdn.bootcss.com/jquery/3.2.1/jquery.min',
+        util: hdjs_path + '/dist/hdjs',
+        hdjs: hdjs_path + '/dist/hdjs',
+        css: hdjs_path + '/dist/static/requirejs/css.min',
+        domReady: hdjs_path + '/dist/static/requirejs/domReady',
     },
     shim: {
         'util': {
-            deps: ['css!app.css']
+            deps: ['css!dist/app.css']
+        },
+        'hdjs': {
+            deps: ['css!dist/app.css']
         },
         'jquery': {
             exports: '$',
